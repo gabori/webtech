c3b76e582ced86b900cb55cc1b4928efc441b1cc
Gábori Péter <gaboripeti95@gmail.com>
2016-12-05 19:14:39 +0100

    commit

--- librarian/workspace/librarian/librarian-web/src/main/java/hu/uni/miskolc/webtech/librarian/controller/IndexController.java
+++ librarian/workspace/librarian/librarian-web/src/main/java/hu/uni/miskolc/webtech/librarian/controller/IndexController.java
@@ -2,23 +2,44 @@ package hu.uni.miskolc.webtech.librarian.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
-
-
 
 @Controller
 public class IndexController {
-	
-	@RequestMapping(value={"", "/", "/home", "/index"})
-	public ModelAndView homePage(){
+
+	@RequestMapping(value = { "", "/", "/home", "/index" })
+	public ModelAndView homePage() {
 		ModelAndView result = new ModelAndView("index");
 		return result;
 	}
 
-	@RequestMapping("/hello")
-	public @ResponseBody String sayHello(){
-		return "Hello World";
+	@RequestMapping(value = "/author")
+	public ModelAndView autherPage() {
+		ModelAndView mav = new ModelAndView("authorspage");
+		return mav;
 	}
 
+	@RequestMapping(value = "/addauthors")
+	public ModelAndView addAuther() {
+		ModelAndView mav = new ModelAndView("addauthor");
+		return mav;
+	}
+
+	@RequestMapping(value = "/updateauthors")
+	public ModelAndView authorFormPage() {
+		ModelAndView mav = new ModelAndView("updateauthor");
+		return mav;
+	}
+
+	@RequestMapping(value = "/book")
+	public ModelAndView bookPage() {
+		ModelAndView mav = new ModelAndView("bookspage");
+		return mav;
+	}
+	
+	@RequestMapping(value = "/contact")
+	public ModelAndView contactPage() {
+		ModelAndView mav = new ModelAndView("contactpage");
+		return mav;
+	}
 }
